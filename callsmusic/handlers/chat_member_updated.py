5d585f72d2b5180456741755345eabe5c3e0e8ea
Roj Serbest <rojserbest@gmail.com>
2021-05-11 02:55:17 +0300

    Fix a small problem in updating admins

--- callsmusic/handlers/chat_member_updated.py
+++ callsmusic/handlers/chat_member_updated.py
@@ -6,24 +6,26 @@ from .. import admins
 
 @Client.on_chat_member_updated()
 async def chat_member_updated(_, chat_member_updated: ChatMemberUpdated):
-    (
-        admins.admins[chat_member_updated.chat.id].append(
-            chat_member_updated.new_chat_member.user.id,
-        )
-    ) if (
+    if chat_member_updated.new_chat_member \
+            and chat_member_updated.old_chat_member:
         (
-            chat_member_updated.new_chat_member.can_manage_voice_chats
-        ) and (
+            admins.admins[chat_member_updated.chat.id].append(
+                chat_member_updated.new_chat_member.user.id,
+            )
+        ) if (
+            (
+                chat_member_updated.new_chat_member.can_manage_voice_chats
+            ) and (
+                (
+                    chat_member_updated.new_chat_member.user.id
+                ) not in admins.admins[chat_member_updated.chat.id]
+            )
+        ) else (
+            admins.admins[chat_member_updated.chat.id].remove(
+                chat_member_updated.new_chat_member.user.id,
+            )
+        ) if (
             (
                 chat_member_updated.new_chat_member.user.id
-            ) not in admins.admins[chat_member_updated.chat.id]
-        )
-    ) else (
-        admins.admins[chat_member_updated.chat.id].remove(
-            chat_member_updated.new_chat_member.user.id,
-        )
-    ) if (
-        (
-            chat_member_updated.new_chat_member.user.id
-        ) in admins.admins[chat_member_updated.chat.id]
-    ) else None
+            ) in admins.admins[chat_member_updated.chat.id]
+        ) else None
